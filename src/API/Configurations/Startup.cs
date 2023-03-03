ed54fc9051f1ac077ea9850f21fd6c47c3d91ad8
Christian Schou <christian.schou@hotmail.com>
2023-03-03 12:08:11 +0100

    Add auto load of configurations
    Added implementation for automatic load of configurations in application. All configuration files must be placed in Configurations and added to the Startup.cs file for being loaded into the application.

---
+++ src/API/Configurations/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace API.Configurations
+{
+    internal static class ConfigureApiBuilderExtension
+    {
+        public static ConfigureHostBuilder AddConfigurations(this ConfigureHostBuilder api)
+        {
+            api.ConfigureAppConfiguration((context, config) =>
+            {
+                const string configurationsDirectory = "Configurations";
+                IHostEnvironment? env = context.HostingEnvironment;
+
+                config.AddJsonFile($"{configurationsDirectory}/appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"{configurationsDirectory}/appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"{configurationsDirectory}/database.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"{configurationsDirectory}/database.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables();
+            });
+            return api;
+        }
+    }
+}
