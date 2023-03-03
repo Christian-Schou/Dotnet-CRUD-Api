95281512ddf2691d682e7625ae29ded24e38278f
Christian Schou <christian.schou@hotmail.com>
2023-03-03 12:08:41 +0100

    Added Serilog + Json options on controllers

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,9 +1,25 @@
+using API.Configurations;
+using Infrastructure.Common;
+using Serilog;
+using System.Text.Json.Serialization;
+
+StaticLogger.EnsureLoggerIsInitialized();
+Log.Information("Starting Web API...");
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Host.AddConfigurations();
+// Add controllers with some extra options
+builder.Services.AddControllers().AddJsonOptions(x =>
+{
+    // Serialize enums as strings in api responses (e.g. Role)
+    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    // Ignore omitted parameters on models to enable optional params (e.g. User update)
+    x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
