44d8b2995e7905cfeca36a9b2ec66e59bfcab2ab
Christian Schou <christian.schou@hotmail.com>
2023-03-04 02:48:26 +0100

    Added initial mapping for default Category to Response DTO

---
+++ src/API/Mappings/Categories/CategoryMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Domain.Entitites.Categories;
+using DTO.Categories;
+
+namespace API.Mappings.Categories
+{
+    public class CategoryMappingProfile : Profile
+    {
+        public CategoryMappingProfile()
+        {
+            CreateMap<Category, CategoryResponseDto>()
+                .ReverseMap()
+                .ForAllMembers(x => x.Condition(
+                    (src, dest, property) =>
+                    {
+                        // Let's ignore both null and empty string properties
+                        if (property == null) return false;
+                        if (property.GetType() == typeof(string) && string.IsNullOrEmpty((string)property)) return false;
+
+                        return true;
+                    }));
+        }
+    }
+}
