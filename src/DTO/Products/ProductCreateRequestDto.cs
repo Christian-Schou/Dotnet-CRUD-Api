068b6d0aa8182e20057cd9b72fa1ef9603de8562
Christian Schou <christian.schou@hotmail.com>
2023-03-05 20:27:29 +0100

    Added Mapping for Products and properties in DTOs

--- src/DTO/Products/ProductCreateRequestDto.cs
+++ src/DTO/Products/ProductCreateRequestDto.cs
@@ -2,5 +2,10 @@
 {
     public class ProductCreateRequestDto
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public Guid CategoryId { get; set; }
     }
 }
