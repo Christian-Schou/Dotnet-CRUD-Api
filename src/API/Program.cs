749a10bb570a76d9eb1a20ec437e95a65c9be27f
Christian Schou <christian.schou@hotmail.com>
2023-03-05 20:49:15 +0100

    Added comments

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Host.AddConfigurations();
+
 // Add controllers with some extra options
 builder.Services.AddControllers().AddJsonOptions(x =>
 {
@@ -21,27 +22,38 @@ builder.Services.AddControllers().AddJsonOptions(x =>
     // Ignore omitted parameters on models to enable optional params (e.g. User update)
     x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
 });
+
+// Add infrastructure services
 builder.Services.AddInfrastructure(builder.Configuration);
+
+// Add mediator and mapper
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Add Endpoint explorer and Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Build the application
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Use Swagger and it's UI
+app.UseSwagger();
+app.UseSwaggerUI();
 
+// Use infrastructure services: middlware, etc...
 app.UseInfrastructure(builder.Configuration);
 
+// Enable HTTPS Redirection
 app.UseHttpsRedirection();
 
+// Enable authorization capabilities
 app.UseAuthorization();
 
+// Map controllers
 app.MapControllers();
 
+// All done - let's run the app / API
+Log.Information("The Web API is now ready to accept incoming requests!");
 app.Run();
